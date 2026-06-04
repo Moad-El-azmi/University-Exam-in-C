6506c572c7b6ac648889caa3f2e97a67cc214def
Moad-El-azmi <momoelazmi@gmail.com>
2026-06-04 19:25:29 +0200

    Update esercizi_iterattivi.c

--- Matrici/esercizi_iterattivi.c
+++ Matrici/esercizi_iterattivi.c
@@ -1,1 +1,21 @@
-// da svolgere gli esercizi 
+/*
+- Esercizio: Compilatore tabelline.
+- Argomento: Cicli iterativi (for).
+- Descrizione: Il programma chiede all'utente un numero intero
+  e ne strampa la tabellina da 1 a 10.
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    int N;
+    
+    printf("Inserisci il numero della tabellina che vuoi: ");
+    scanf("%d", &N);
+    
+    for(int i=1; i <=10; i++){
+        printf("%d\n", N * i);
+    }
+    return 0;
+}
